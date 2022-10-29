04553bb05409b3f1fcae18d0d45abe4db3186b16
LetMeFly666 <814114971@qq.com>
2022-10-29 15:32:15 +0800

    修改了nginx后 Invalid HTTP_HOST header: 'back.share1024.letmefly.xyz'. You may need to add 'back.share1024.letmefly.xyz' to ALLOWED_HOSTS.

--- back/Let1024/settings.py
+++ back/Let1024/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = Secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['back.share1024.letmefly.xyz']
 
 
 # Application definition
