855202e38d8c1f0efcf015de596bb75926048787
LetMeFly666 <814114971@qq.com>
2022-10-30 20:29:12 +0800

    dbg

--- back/Apps/Functions/Card.py
+++ back/Apps/Functions/Card.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-10-28 18:04:22
 LastEditors: LetMeFly
-LastEditTime: 2022-10-30 20:26:02
+LastEditTime: 2022-10-30 20:29:08
 '''
 from django.http import JsonResponse
 from django.shortcuts import redirect
@@ -176,6 +176,10 @@ def share(request):
     th = got.first().th
     user.update(lastGot=0, shareNum=user.first().shareNum + 1)
     newCard = models.Cards.objects.create(shareBy=username, cardIs=cardType, leetcodeURL=leetcodeURL)
+    print(newCard)
+    print(type(newCard))
+    for i in newCard:
+        print(i, type(i))
     newCardID = newCard.objects.first().cardID
     print(newCardID)
     print(type(got))
